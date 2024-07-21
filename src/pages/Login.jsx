77c3d442a9b60dded283d17b89574736a00b0527
Lee Yeon <rofttm0033@gmail.com>
2024-07-21 13:48:09 +0900

    [feature] login 컴포넌트 추가

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+//목적: 로그인 버튼 클릭 시 작동할 로그인 화면 구현
+//기능: 사용자 로그인 정보입력과 검증
+//2024.07.21 데이-이연
+
+import React, { useState } from "react";
+import styled from "styled-components"
+
+const Login = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default Login;
