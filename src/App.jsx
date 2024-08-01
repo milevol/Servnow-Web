1128a71f26c391a1249fdd938642cc03f789c36f
Lee Yeon <rofttm0033@gmail.com>
2024-08-01 10:00:37 +0900

    [feature] 내정보수정 ui 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import AnswerEndPage from "./pages/AnswerEndPage";
 import AnswerPage from "./pages/AnswerPage";
 import SignUpPage from "./pages/signup/SignUpPage";
+import MyInfoModifyKakaoPage from "./pages/mypage/MyInfoModifyKakaoPage";
+import MyInfoModifyPage from "./pages/mypage/MyInfoModifyPage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/answerend" element={<AnswerEndPage />} />
       <Route path="/answer" element={<AnswerPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/myinfo-modify-kakao" element={<MyInfoModifyKakaoPage />} />
+      <Route path="/myinfo-modify" element={<MyInfoModifyPage />} />
     </Routes>
   );
 }
