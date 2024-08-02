9f9fb53b4f562b92a06d2030d7309abe09832727
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2024-08-02 18:54:19 +0900

    [feature] add draft of survey distribution modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AnswerEndPage from "./pages/AnswerEndPage";
 import AnswerPage from "./pages/AnswerPage";
 import SignUpPage from "./pages/signup/SignUpPage";
 import MyPage from "./pages/MyPage";
+import SurveyModalPage from "./pages/SurveyModalPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/answer" element={<AnswerPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/mypage" element={<MyPage />} />
+      <Route path="/surveymodal" element={<SurveyModalPage />} />
     </Routes>
   );
 }
