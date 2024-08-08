b6f6a3a9971b93246d90717ce74ba6f6cd6eeb54
tl1l1l1s <shj721553@gmail.com>
2024-08-08 15:04:59 +0900

    fix: delete menu icon, adjust padding, add email input for non-login user

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AnswerEndPage from "./pages/AnswerEndPage";
 import AnswerPage from "./pages/AnswerPage";
 import SignUpPage from "./pages/signup/SignUpPage";
 import MyPage from "./pages/MyPage";
+import AnswerStartPage from "./pages/AnswerStartPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/answer" element={<AnswerPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/mypage" element={<MyPage />} />
+      <Route path="/answerstart" element={<AnswerStartPage />} />
     </Routes>
   );
 }
