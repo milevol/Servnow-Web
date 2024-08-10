e4871289d4446841788ec1be2fa73bea9274b60d
Lee Yeon <rofttm0033@gmail.com>
2024-08-11 05:18:05 +0900

    [feature] 인증확인 버튼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { Route, Routes } from "react-router";
 import AnswerEndPage from "./pages/AnswerEndPage";
 import AnswerPage from "./pages/AnswerPage";
 import SignUpPage from "./pages/signup/SignUpPage";
-import MyInfoModifyPage from "./pages/mypage/MyInfoModifyPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MyInfoModifyPage />}/>
+      <Route path="/" />
       <Route path="/answerend" element={<AnswerEndPage />} />
       <Route path="/answer" element={<AnswerPage />} />
       <Route path="/signup" element={<SignUpPage />} />
