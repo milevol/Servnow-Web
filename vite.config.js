fc200c20532f76fd48b8863ee2f6beffc560debc
tl1l1l1s <shj721553@gmail.com>
2024-08-21 17:44:40 +0900

    [chore]change base api url to env

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://www.servnow.kro.kr",
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": env.VITE_API_BASE_URL,
+      },
     },
-  },
-});
+  });
+};
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": import.meta.env.VITE_API_URL,
+//     },
+//   },
+// });
